962970e386d51c1ce3d2e3292693ed6449b4d280
yukio <yukio.sugiyama.ster@gmail.com>
2019-09-11 03:06:04 +0900

    update

--- get_data.py
+++ get_data.py
@@ -1,15 +1,15 @@
 import datetime
 
 import mch_apis
-import mch_heroe
-import mch_extension
+import mch_hero
+import mch_exte
 
 class GetData:
 
     def __init__(self):
         self.mch = mch_apis.MCHAPI()
-        self.hero = mch_heroe.MCHHeroeData()
-        self.exte = mch_extension.MCHExtensionData()
+        self.hero = mch_hero.MCHHeroData()
+        self.exte = mch_exte.MCHExteData()
 
 
     def get_eth(self, user_id):
@@ -124,7 +124,7 @@ class GetData:
             lv = self.hero.get_lv()
             url = self.hero.get_url()
 
-            hero_data = {'rarity':rarity, 'name':name, 'id':id, 'lv':lv}
+            hero_data = {'rarity':rarity, 'name':name, 'id':id, 'lv':lv, 'url':url}
 
             hero_data_set.append(hero_data)
 
@@ -151,99 +151,14 @@ class GetData:
             lv = self.exte.get_lv()
             url = self.exte.get_url()
 
-            exte_data = {'rarity':rarity, 'name':name, 'id':id, 'lv':lv}
+            exte_data = {'rarity':rarity, 'name':name, 'id':id, 'lv':lv, 'url':url}
 
             exte_data_set.append(exte_data)
 
         return exte_data_set
 
+
     def get_hero_sold(self, since = '', until = ''):
-
-        hero_sold = self.mch.get_hero_sold_trades(since, until)
-
-        hero_sold_set = []
-        for x in hero_sold:
-            id = x['hero_id']
-            time = x['sold_at']
-            sold_price = x['price']
-            ce = x['ce']
-            seller_id = x['seller_id']
-            buyer_id = x['buyer_id']
-
-#            seller_name = self.get_user_name(seller_id)
-#            buyer_name = self.get_user_name(buyer_id)
-
-
-            sold_time = datetime.datetime.fromtimestamp(time)
-
-            hero_metadata = self.get_hero_metadata(id)
-            self.hero.set_data(hero_metadata)
-
-            type = self.hero.get_type()
-
-            hero_type_metadata = self.get_hero_type_metadata(type)
-            self.hero.set_type_data(hero_type_metadata)
-
-            name = self.hero.get_name_ja()
-
-            hero_sold = {'sold_time':sold_time
-                        , 'name':name
-                        , 'sold_price':sold_price
-                        , 'ce':ce
-                        , 'seller_id':seller_id
-                        , 'buyer_id':buyer_id
-                        }
-
-            hero_sold_set.append(hero_sold)
-
-#            print('{:0} {:1} 価格:{:2,} CE:{:3,} 売:{:4} 買:{:5}'.format(sold_time, name, sold_price, ce, seller_id, buyer_id))
-
-        return hero_sold_set
-
-    def get_exte_sold(self, since = '', until = ''):
-
-        exte_sold = self.mch.get_extension_sold_trades(since, until)
-
-        exte_sold_set = []
-        for x in exte_sold:
-            id = x['extension_id']
-            time = x['sold_at']
-            sold_price = x['price']
-            ce = x['ce']
-            seller_id = x['seller_id']
-            buyer_id = x['buyer_id']
-
-#            seller_name = self.get_user_name(seller_id)
-#            buyer_name = self.get_user_name(buyer_id)
-
-
-            sold_time = datetime.datetime.fromtimestamp(time)
-
-            exte_metadata = self.get_exte_metadata(id)
-            self.exte.set_data(exte_metadata)
-
-            type = self.exte.get_type()
-
-            exte_type_metadata = self.get_exte_type_metadata(type)
-            self.exte.set_type_data(exte_type_metadata)
-
-            name = self.exte.get_name_ja()
-
-            exte_sold = {'sold_time':sold_time
-                        , 'name':name
-                        , 'sold_price':sold_price
-                        , 'ce':ce
-                        , 'seller_id':seller_id
-                        , 'buyer_id':buyer_id
-                        }
-
-            exte_sold_set.append(exte_sold)
-
-#            print('{:0} {:1} 価格:{:2,} CE:{:3,} 売:{:4} 買:{:5}'.format(sold_time, name, sold_price, ce, seller_id, buyer_id))
-
-        return exte_sold_set
-
-    def get_hero_sold_csv(self, since = '', until = ''):
 
         hero_sold = self.mch.get_hero_sold_trades(since, until)
 
@@ -256,43 +171,24 @@ class GetData:
             ce = x['ce']
             seller_id = x['seller_id']
             buyer_id = x['buyer_id']
-
-#            seller_name = self.get_user_name(seller_id)
-#            buyer_name = self.get_user_name(buyer_id)
-
-            sold_time = datetime.datetime.fromtimestamp(sold_at)
-
-#            hero_metadata = self.get_hero_metadata(hero_id)
-#            self.hero.set_data(hero_metadata)
-
-#            rarity = self.hero.get_rarity()
-#            type = self.hero.get_type()
-
-#            hero_type_metadata = self.get_hero_type_metadata(type)
-#            self.hero.set_type_data(hero_type_metadata)
-
-#            name = self.hero.get_name_ja()
+            sold_time = datetime.datetime.fromtimestamp(sold_at).strftime("%Y/%m/%d %H:%M:%S")
 
             hero_sold = {'trade_id':trade_id
-                        , 'hero_id':hero_id
+                        , 'asset_id':hero_id # exteと同期するためkey値は「asset_id」で統一
                         , 'sold_at':sold_at
                         , 'price':price
                         , 'ce':ce
                         , 'seller_id':seller_id
                         , 'buyer_id':buyer_id
                         , 'sold_time':sold_time
-#                        , 'rarity':rarity
-#                        , 'name':name
-#                        , 'sold_time':sold_time
-#                        , 'seller_name':seller_name
-#                        , 'buyer_name':buyer_name
                         }
 
             hero_sold_set.append(hero_sold)
 
         return hero_sold_set
 
-    def get_exte_sold_csv(self, since = '', until = ''):
+
+    def get_exte_sold(self, since = '', until = ''):
 
         exte_sold = self.mch.get_extension_sold_trades(since, until)
 
@@ -305,36 +201,16 @@ class GetData:
             ce = x['ce']
             seller_id = x['seller_id']
             buyer_id = x['buyer_id']
-
-#            seller_name = self.get_user_name(seller_id)
-#            buyer_name = self.get_user_name(buyer_id)
-
-            sold_time = datetime.datetime.fromtimestamp(sold_at)
-
-#            exte_metadata = self.get_exte_metadata(exte_id)
-#            self.exte.set_data(exte_metadata)
-
-#            rarity = self.exte.get_rarity()
-#            type = self.exte.get_type()
-
-#            exte_type_metadata = self.get_exte_type_metadata(type)
-#            self.exte.set_type_data(exte_type_metadata)
-
-#            name = self.exte.get_name_ja()
+            sold_time = datetime.datetime.fromtimestamp(sold_at).strftime("%Y/%m/%d %H:%M:%S")
 
             exte_sold = {'trade_id':trade_id
-                        , 'exte_id':exte_id
+                        , 'asset_id':exte_id # heroと同期するためkey値は「asset_id」で統一
                         , 'sold_at':sold_at
                         , 'price':price
                         , 'ce':ce
                         , 'seller_id':seller_id
                         , 'buyer_id':buyer_id
                         , 'sold_time':sold_time
-#                        , 'rarity':rarity
-#                        , 'name':name
-#                        , 'sold_time':sold_time
-#                        , 'seller_name':seller_name
-#                        , 'buyer_name':buyer_name
                         }
 
             exte_sold_set.append(exte_sold)
